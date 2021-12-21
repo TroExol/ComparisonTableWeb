6ec63525efb8e5e3891536c601267cb186dc0502
TroExol <zeidxol@gmail.com>
2021-12-21 21:41:21 +0500

    fix for build

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {HashRouter, Route, Routes} from 'react-router-dom';
 
 import store from './store';
 
@@ -12,7 +12,7 @@ import Navbar from './components/Navbar';
 const App = () => {
     return (
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
                 <Navbar />
                 <div>
                     <Routes>
@@ -22,7 +22,7 @@ const App = () => {
                         <Route path="/lootfarm2itrade" element={<LootfarmToItrade />} />
                     </Routes>
                 </div>
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     );
 };
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const Axios = require('axios');
 const app = express();
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     next();
